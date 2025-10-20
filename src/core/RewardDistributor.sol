904274e79501e7b7b98d31c0f3cdac81c9f1dfd0
Roman Kolpakov <rkolpakov94@gmail.com>
2025-10-20 11:22:21 +0300

    feat: add reward distributor contract

---
+++ src/core/RewardDistributor.sol
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract RewardDistributor {
+    using SafeERC20 for IERC20;
+
+    uint256 public constant MAX_BPS = 10_000;
+
+    struct Recipient {
+        address account;
+        uint256 basisPoints;
+    }
+
+    Recipient[] public recipients;
+
+    event RewardsDistributed(address indexed token, uint256 totalAmount);
+    event RecipientPaid(
+        address indexed recipient,
+        address indexed token,
+        uint256 amount
+    );
+
+    error InvalidRecipientsLength();
+    error InvalidBasisPointsSum();
+    error ZeroAddress();
+    error ZeroBasisPoints();
+    error NoBalance();
+
+    constructor(address[] memory _recipients, uint256[] memory _basisPoints) {
+        if (_recipients.length != _basisPoints.length) {
+            revert InvalidRecipientsLength();
+        }
+
+        if (_recipients.length == 0) {
+            revert InvalidRecipientsLength();
+        }
+
+        uint256 totalBps = 0;
+
+        for (uint256 i = 0; i < _recipients.length; i++) {
+            if (_recipients[i] == address(0)) {
+                revert ZeroAddress();
+            }
+
+            if (_basisPoints[i] == 0) {
+                revert ZeroBasisPoints();
+            }
+
+            recipients.push(
+                Recipient({
+                    account: _recipients[i],
+                    basisPoints: _basisPoints[i]
+                })
+            );
+
+            totalBps += _basisPoints[i];
+        }
+
+        if (totalBps != MAX_BPS) {
+            revert InvalidBasisPointsSum();
+        }
+    }
+
+    function distribute(address token) external {
+        IERC20 tokenContract = IERC20(token);
+        uint256 balance = tokenContract.balanceOf(address(this));
+
+        if (balance == 0) {
+            revert NoBalance();
+        }
+
+        for (uint256 i = 0; i < recipients.length; i++) {
+            Recipient memory recipient = recipients[i];
+
+            uint256 amount = (balance * recipient.basisPoints) / MAX_BPS;
+
+            if (amount > 0) {
+                tokenContract.safeTransfer(recipient.account, amount);
+
+                emit RecipientPaid(recipient.account, token, amount);
+            }
+        }
+
+        emit RewardsDistributed(token, balance);
+    }
+
+    function getRecipientsCount() external view returns (uint256) {
+        return recipients.length;
+    }
+
+    function getRecipient(
+        uint256 index
+    ) external view returns (address account, uint256 basisPoints) {
+        Recipient memory recipient = recipients[index];
+        return (recipient.account, recipient.basisPoints);
+    }
+
+    function getAllRecipients() external view returns (Recipient[] memory) {
+        return recipients;
+    }
+}
